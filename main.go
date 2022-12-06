24f0711629e1dc2fee70e5cc24e44d8f4f043626
Yuorei <yuorei71@gmail.com>
2022-12-06 13:44:44 +0900

    create controller

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
-import(
-	"net/http"
+
+import (
+	//"net/http"
+	"subculture_ranking/controller"
+
 	"github.com/gin-gonic/gin"
 )
-func mian(
 
-)
+func main() {
+	r := gin.Default()
+	r.GET("/ranking", controller.GetRanking)
+	r.Run() // 0.0.0.0:8080 でサーバーを立てます。
+}
