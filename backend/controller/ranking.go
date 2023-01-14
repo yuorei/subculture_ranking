35eb91dc3fccdae8ee8836cad4712d165a161754
yuorei <yuorei71@gmail.com>
2023-01-14 23:23:39 +0900

    GetRanking

--- backend/controller/ranking.go
+++ backend/controller/ranking.go
@@ -1,62 +1,20 @@
 package controller
 
 import (
-	"strconv"
-	"subculture_ranking/operateDb"
+
+	"subculture_ranking/db"
+	"subculture_ranking/db/table"
+
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 )
 
-// /ranking/:user-id/:category-id
-func GetRanking(c *gin.Context) {
-	var req, res operateDb.Ranking
-	req.User_id, _ = strconv.Atoi(c.Param("user-id"))
-	req.Category_id, _ = strconv.Atoi(c.Param("category-id"))
-	// todo DBからselectしてJSONで返す
+// /ranking
+func GetAllRankings(c *gin.Context) {
+	var res []table.User
+
+	db := db.ConnectDB()
+	db.Find(&res)
+	// todo 本当に必要なものだけを返す
 	c.JSON(200, res)
-}
-
-// /res/register
-func PostRegister(c *gin.Context) {
-	var req, res operateDb.Ranking
-	err := c.BindJSON(&req)
-	if err != nil {
-		c.JSON(400, err)
-		return
-	}
-	// todo DBにinsert
-	res.Uuid = uuid.New().String()
-	res.User_id = req.User_id
-	res.Category_id = req.Category_id
-	res.Title = req.Title
-	res.Image_base64=req.Image_base64
-	res.Comment = req.Comment
-	res.Rank = req.Rank
-	c.JSON(200, res)
-}
-
-func PutRegister(c *gin.Context) {
-	var req, res operateDb.Ranking
-	err := c.BindJSON(&req)
-	if err != nil {
-		c.JSON(400, err)
-		return
-	}
-	// todo DBにupdata
-	res.Uuid = c.Param("uuid")
-	res.User_id = req.User_id
-	res.Category_id = req.Category_id
-	res.Title = req.Title
-	res.Image_base64=req.Image_base64
-	res.Comment = req.Comment
-	res.Rank = req.Rank
-	c.JSON(200, res)
-}
-
-func DeleteRegister(c *gin.Context){
-	// uuid:=c.Param("uuid")
-	// todo DBから削除
-
-	c.JSON(200,nil)
 }
