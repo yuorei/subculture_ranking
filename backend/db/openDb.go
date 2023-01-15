f5753acdac7a5f93f5b93980dc8b9bb41bcea943
yuorei <yuorei71@gmail.com>
2023-01-15 13:22:44 +0900

    テーブル作成

--- backend/db/openDb.go
+++ backend/db/openDb.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"fmt"
 	"os"
 	"subculture_ranking/db/table"
 
@@ -9,45 +8,41 @@ import (
 	"gorm.io/gorm"
 )
 
-var db *gorm.DB 
+var db *gorm.DB
 
 func Init() *gorm.DB {
-	fmt.Println("jeforaghokaehrgohreigheaihgaophgoahfo")
-	fmt.Println(os.Getenv("DB_HOST"))
+
+	var err error
+
 	dsn := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASS") + " dbname=" + os.Getenv("DB_NAME") + " port=" + os.Getenv("DB_PORT") + " sslmode=disable TimeZone=" + os.Getenv("TIMEZONE")
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
 	if err != nil {
-		fmt.Println(err)
 		panic(err)
 	}
-	// todo tableを作る
-	err=db.AutoMigrate(&table.Comment{})
-	if err!=nil{
-		panic(err)
-	}
+
 	CreateTable()
 	return db
 }
 
-func CreateTable(){
-
-	if err:=db.AutoMigrate(&table.Comment{}); err!=nil{
+func CreateTable() {
+	if err := db.AutoMigrate(&table.Comment{}); err != nil {
 		panic(err)
 	}
-	if err:=db.AutoMigrate(&table.Category{}); err!=nil{
+	if err := db.AutoMigrate(&table.Category{}); err != nil {
 		panic(err)
 	}
-	if err:=db.AutoMigrate(&table.Genre{}); err!=nil{
+	if err := db.AutoMigrate(&table.Genre{}); err != nil {
 		panic(err)
 	}
-	if err:=db.AutoMigrate(&table.Ranking_list{}); err!=nil{
+	if err := db.AutoMigrate(&table.RankingList{}); err != nil {
 		panic(err)
 	}
-	if err:=db.AutoMigrate(&table.User{}); err!=nil{
+	if err := db.AutoMigrate(&table.User{}); err != nil {
 		panic(err)
 	}
 }
 
-func ConnectDB() *gorm.DB{
+func ConnectDB() *gorm.DB {
 	return db
 }
