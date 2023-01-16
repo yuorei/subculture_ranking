b4c643fd37d51e78375c21df6e6730ad46fb34be
yuorei <yuorei71@gmail.com>
2023-01-16 09:56:07 +0900

    formからのuser登録入力をPOSTで送信

---
+++ backend/frontend/templates/js/user_register.js
@@ -0,0 +1,25 @@
+const url = '/users';
+
+const postFetch = () => {
+    const data = {
+        Name:  document.forms['form1'].elements['name'].value,
+    };
+
+    fetch( url, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+    })
+    // error処理
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+    };
+
+btn.addEventListener('click', postFetch, false);
