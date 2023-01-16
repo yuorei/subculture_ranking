549b1102e9a8b129755c3655b079fe6d939ee1a4
yuorei <yuorei71@gmail.com>
2023-01-16 10:08:51 +0900

    エンドポイント追加

--- backend/main.go
+++ backend/main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"subculture_ranking/controller"
+	"subculture_ranking/db"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/ranking/:user-id/:category-id", controller.GetRanking)
-	r.POST("/ranking/register",controller.PostRegister)
-	r.PUT("/ranking/register/:uuid",controller.PutRegister)
-	r.DELETE("/ranking/register/:uuid",controller.DeleteRegister)
+	r.Static("/js", "./frontend/templates/js")
+	r.LoadHTMLGlob("./frontend/templates/*.html")
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	_ = db.Init()
+	r.GET("/", controller.GetIndex)
+	r.GET("/all-users",controller.GetAllUsersHTML)
+	r.GET("/ranking", controller.GetAllRankingUsersHTML)
+	r.GET("/ranking-user-data", controller.GetAllRankingUsers)
+	r.GET("/ranking/:user-id", controller.GetUserRankings)
+	r.POST("/ranking/", controller.PostUserRanking)
+	r.GET("/users", controller.GetUserProfile)
+	r.POST("/users", controller.PostUserProfile)
 	r.Run()
 }
