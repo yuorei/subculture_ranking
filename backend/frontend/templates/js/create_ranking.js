e270960d691380f001b9e8e898bbf0bd2dc68ffe
yuorei <yuorei71@gmail.com>
2023-01-16 09:59:34 +0900

    formからのranking登録入力をPOSTで送信

---
+++ backend/frontend/templates/js/create_ranking.js
@@ -0,0 +1,25 @@
+const url = '/ranking';
+
+const postFetch = () => {
+    const data = {
+        Title:  document.forms['form1'].elements['name'].value,
+    };
+
+    fetch( url, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+    })
+    // error処理
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+    };
+
+btn.addEventListener('click', postFetch, false);
